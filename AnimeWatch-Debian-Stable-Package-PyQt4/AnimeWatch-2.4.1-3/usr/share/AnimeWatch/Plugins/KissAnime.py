d05094c232e5fddcf8434ed3ad066f4cd0c785af
kanishka-linux <gnulinux.fan.10@gmail.com>
2016-07-26 17:05:02 +0530

    UnicodeEncodeError Fixed

--- AnimeWatch-Debian-Stable-Package-PyQt4/AnimeWatch-2.4.1-3/usr/share/AnimeWatch/Plugins/KissAnime.py
+++ AnimeWatch-Debian-Stable-Package-PyQt4/AnimeWatch-2.4.1-3/usr/share/AnimeWatch/Plugins/KissAnime.py
@@ -101,7 +101,10 @@ def ccurl(url):
 		cloudfare(url,'')
 		c.setopt(c.COOKIEFILE, '/tmp/AnimeWatch/kcookie.txt')
 	url = str(url)
-	c.setopt(c.URL, url)
+	try:
+		c.setopt(c.URL, url)
+	except UnicodeEncodeError:
+		c.setopt(c.URL, url.encode('utf-8'))
 	storage = BytesIO()
 	if curl_opt == '-o':
 		f = open(picn_op,'wb')
